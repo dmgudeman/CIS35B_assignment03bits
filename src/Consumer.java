47d022dd925efe7511fbaf9f816013803f576679
David Gudeman <davidmgudeman@gmail.com>
2015-07-28 18:21:19 -0700

    bitSet configuration is fully working, needs comments and cleanup

--- src/Consumer.java
+++ src/Consumer.java
@@ -75,11 +75,42 @@ class Consumer implements Runnable
     }
     public void robotWork (BitSet bitSet)
     {
+        try
+        {
+            System.out.println("Robot arm number" +  bitSet.get(0, 4) + " is working.");
 
-            System.out.println("bitSet.get(0, 5) " + bitSet.get(0, 4));
-            System.out.println("bitSet.get(5, 9) " + bitSet.get(5,9));
-            System.out.println("bitSet.get(9, 17) " + bitSet.get(9, 17));
-            System.out.println("bitSet " + bitSet);
+            if (bitSet.get(5)){ System.out.println("State: ON");}
+            else { System.out.println("State: Off");}
+
+
+            if (bitSet.get(6)){System.out.println("Speed: High");}
+            else {System.out.println("Speed: Low");}
+
+            if (bitSet.get(7)){ System.out.println("Rotation: Clockwise");}
+            else {System.out.println("Rotation: Counterclockwise");}
+
+            if (bitSet.get(8)){ System.out.println("Direction: Up");}
+            else {System.out.println("Direction: Down");}
+
+            BitSet timeBS = new BitSet();
+            timeBS = bitSet.get(9, 17);
+            int value = 0;for (int k = 0; k<9; ++k)
+            {
+                  value += timeBS.get(k) ? (1<< k) : 0;
+            }
+            System.out.println("MILLISECONDS " + value);
+            System.out.println("\n");
+
+            Thread.sleep(value);
+            }
+        catch (InterruptedException e)
+        {
+            System.out.println("Interrupted Exception caught");
+        }
+
+      //      System.out.println("bitSet.get(5, 9) " + bitSet.get(5,9));
+       //     System.out.println("bitSet.get(9, 17) " + bitSet.get(9, 17));
+        //    System.out.println("bitSet " + bitSet);
           //  System.out.println("Sleeping " + command[5] + "milliseconds");
          //   Thread.sleep(Integer.parseInt(command[5]));
     }
